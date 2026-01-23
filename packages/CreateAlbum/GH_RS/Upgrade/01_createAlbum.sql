bb07b40a49ad72d54a29c9c26865eff89c612568
sergioldbm <sergiol@dbmaestro.com>
2026-01-23 14:28:36 -0300

    create manifest

--- packages/CreateAlbum/GH_RS/Upgrade/01_createAlbum.sql
+++ packages/CreateAlbum/GH_RS/Upgrade/01_createAlbum.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[Album]
 (
     [AlbumId] INT NOT NULL,
-    [Title] NVARCHAR(169) NOT NULL,
+    [Title] NVARCHAR(170) NOT NULL,
     [ArtistId] INT NOT NULL,
     CONSTRAINT [PK_Album] PRIMARY KEY CLUSTERED ([AlbumId])
 ); 
